7621fa5679137d4c7a0f2eae89e82120444aaa51
Kolade Arisekola <arisekolakolade1@gmail.com>
2018-07-14 20:45:01 +0100

    Udacity Android Basics Project

---
+++ app/src/main/java/com/raykone/miwokk/PhrasesActivity.java
@@ -0,0 +1,22 @@
+package com.raykone.miwokk;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class PhrasesActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, new PhrasesFragment())
+                .commit();
+    }
+
+
+
+
+}
